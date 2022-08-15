35ff6df93e8ec50ac050be45d3a5787003d47f7f
amanj ghaderi <ghaderi.amanj@gmail.com>
2022-08-15 17:41:38 +0430

    post details updated

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,6 @@
 import React from "react";
 import PostContent from "../../components/posts/posts-detail/PostContent";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 function SingleDetailPage() {
   return (
@@ -8,5 +9,34 @@ function SingleDetailPage() {
     </div>
   );
 }
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
+};
 
 export default SingleDetailPage;
